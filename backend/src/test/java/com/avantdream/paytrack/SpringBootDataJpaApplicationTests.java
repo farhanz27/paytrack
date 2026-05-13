15280d4f9eeebc8e16f74b19eda5642b5b526a1b
Han <farhanzaid27@gmail.com>
2026-05-14 01:45:41 +0800

    chore: add Maven wrapper, Dockerfile, integration test, and Docker Compose

---
+++ backend/src/test/java/com/avantdream/paytrack/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avantdream.paytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
